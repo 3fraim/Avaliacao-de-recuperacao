0e82d95475afa63bf8204e50a5722c11e5217613
Efraim Ferreira <128260580+3fraim@users.noreply.github.com>
2023-07-05 08:32:52 -0300

    Update Main.java

--- avaliacao/src/main/java/main/Main.java
+++ avaliacao/src/main/java/main/Main.java
@@ -24,7 +24,7 @@ import cargosconfianca.Gerente;
 public class Main {
 
     public static void main(String[] args) {
-     Advogado advogado = new advogado("1", "Felipe", "000.000.000-00", "00000000000", new Endereco("Rua 1", "1", "Bairro 1", "11111-111", "Salvador", UnidadeFederativa.BAHIA), Setor.ENGENHARIA, Genero.MASCULINO, 1000, EstadoCivil.SOLTEIRO, LocalDate.of(2003, Month.JUNE, 6));
+     Advogado advogado = new advogado("1", "Felipe", "000.000.000-00", "00000000000", new Endereco("Rua 1", "1", "Bairro 1", "11111-111", "Salvador", UnidadeFederativa.BAHIA), Setor.JURIDICO, Genero.MASCULINO, 1000, EstadoCivil.SOLTEIRO, LocalDate.of(2003, Month.JUNE, 6));
        Motorista motorista = new motorista("22222", "Andrea", "000.000.000-00", "111111111", new Endereco("Rua 2", "2", "Bairro 2", "33333-333", "Guarulhos", UnidadeFederativa.SAO_PAULO), Setor.OPERACOES, Genero.FEMININO, 2000, EstadoCivil.DIVORCIADO, LocalDate.of(2002, Month.JUNE, 6));
        Gerente gerente = new Gerente(Bonificacao.GERENTE, "Adolfo", "000.000.000-00", "00000000", new Endereco("Rua 3", "3", "Bairro 3", "33333-333", "Copacabana", UnidadeFederativa.RIO_DE_JANEIRO), Setor.OPERACOES, Genero.MASCULINO, 2000, EstadoCivil.VIUVO, LocalDate.of(2001, Month.MARCH, 6));
        Diretor diretor = new Diretor(Bonificacao.DIRETOR, "Rosa", "000.000.000-00", "0000000000", new Endereco("Rua 4", "4", "Bairro 4", "44444-444", "Camaçari", UnidadeFederativa.BAHIA), Setor.OPERACOES, Genero.FEMININO, 2000, EstadoCivil.CASADO, LocalDate.of(2000, Month.MARCH, 6));
